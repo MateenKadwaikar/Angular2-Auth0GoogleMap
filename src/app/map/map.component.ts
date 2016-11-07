290d9ec545f1619a8cb0e6f46b7dcebf26061c1f
Mateen <mateen.kadwaikar@provintl.com>
2016-11-07 17:51:47 +0530

    Map Info Window shows the address of the lat and long.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Auth } from '../service/auth0.service';
 
+declare var google: any;
+
 @Component({
   selector: 'app-my-google-map',
   templateUrl: 'map.component.html',
@@ -11,20 +13,22 @@ export class MapComponent implements OnInit {
 
   public lat: number;
   public lng: number;
-  public zoom: number = 16;
+  public zoom: number = 14;
   private userName: string;
   private userImage: string = '../../assets/profile_default.jpg';
+  public address: string;
 
   ngOnInit() {
     this.setLatandLng();
     this.getUserDetails();
   }
 
-  constructor(private auth: Auth) { }
+  constructor(private auth: Auth) {}
 
   setLatandLng() {
     if (navigator) {
-      navigator.geolocation.getCurrentPosition((data) => {
+      navigator.geolocation.getCurrentPosition(
+      (data) => {
         this.lat = data.coords.latitude;
         this.lng = data.coords.longitude;
       });
@@ -34,8 +38,33 @@ export class MapComponent implements OnInit {
   getUserDetails() {
     if (localStorage.length > 0) {
       this.userName = JSON.parse(localStorage.getItem('profile')).name;
-      this.userImage = JSON.parse(localStorage.getItem('profile'))['picture'] ;
+      this.userImage = JSON.parse(localStorage.getItem('profile')).picture;
     }
+  }
+
+  clickedMarker(lat, lng) {
+    this.getAddress(lat, lng);
+  }
+
+  zoomChange(e) {
+    console.log(e);
+  }
+
+  mapClicked(e) {
+    console.log(e);
+  }
+
+  getAddress(lat, lng) {
+
+    let location = new google.maps.LatLng(lat, lng);
+    let geocoder = new google.maps.Geocoder();
+    geocoder.geocode(
+      { location: location },
+      (res) => {
+        console.log(res);
+        this.address = res[0].formatted_address;
+      }
+    );
   }
 
 }
